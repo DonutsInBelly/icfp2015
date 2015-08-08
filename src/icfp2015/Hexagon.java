18e12f15b73fdbe83a54127265b35ca8f9b4fd5b
Carlin Au <carlinau@gmail.com>
2015-08-08 12:35:06 -0400

    Fixed Constructor

--- src/icfp2015/Hexagon.java
+++ src/icfp2015/Hexagon.java
@@ -5,9 +5,10 @@ public class Hexagon
 	public Boolean full;
 	public double x;
 	public double y;
-	public Hexagon(double x, double y)
+	public Hexagon(double x, double y, Boolean full)
 	{
 		x = this.x;
 		y = this.y;
+		full = this.full;
 	}
 }
