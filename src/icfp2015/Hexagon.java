0df7c1fc7de05fbaefc7e163ecee685eab0b3a5d
Carlin Au <carlinau@gmail.com>
2015-08-08 13:11:28 -0400

    comments in Hexagon

--- src/icfp2015/Hexagon.java
+++ src/icfp2015/Hexagon.java
@@ -1,10 +1,16 @@
 package icfp2015;
 
+/*
+ * Hexagon(x,y,full)
+ * -Contain x,y coordinates for board; x and y are doubles
+ * -full is a boolean: true for Full, False for empty
+ */
 public class Hexagon 
 {
 	public Boolean full;
 	public double x;
 	public double y;
+	
 	public Hexagon(double x, double y, Boolean full)
 	{
 		x = this.x;
