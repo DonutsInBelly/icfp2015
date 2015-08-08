9245c3a5106d7bda7e93d74211ab8db06cc15393
Carlin Au <carlinau@gmail.com>
2015-08-08 12:29:09 -0400

    pls work

---
+++ src/icfp2015/Hexagon.java
@@ -0,0 +1,13 @@
+package icfp2015;
+
+public class Hexagon 
+{
+	public Boolean full;
+	public double x;
+	public double y;
+	public Hexagon(double x, double y)
+	{
+		x = this.x;
+		y = this.y;
+	}
+}
