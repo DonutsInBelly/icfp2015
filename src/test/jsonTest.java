c3e0d45fc523c6b9e7c8d5938ef85649ff95b3e5
Carlin Au <carlinau@gmail.com>
2015-08-08 20:59:08 -0400

    Changes: -Started writing jsonTest.java

--- src/test/jsonTest.java
+++ src/test/jsonTest.java
@@ -1,8 +1,15 @@
 package test;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class jsonTest 
 {
 	public static void main (String[] args)
 	{
+		File json;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter File Path: ");
+		//json = scan.next();
 	}
 }
