404600db65ed59838c27a090dedbf89fdaa300ce
Harsh Shah <hms96@scarletmail.rutgers.edu>
2015-08-09 15:26:32 -0400

    Base Frame

--- src/GUI/BoardView.java
+++ src/GUI/BoardView.java
@@ -3,10 +3,21 @@ package GUI;
 import javax.swing.*;
 
 
-public class BoardView {
-	
-	public JFrame BoardWindow;
+public class BoardView extends JFrame {
 	
 	
 	
+	public BoardView(){
+		this.setVisible(true);
+		this.setResizable(true);
+		this.setTitle("ICFP Game");
+		this.setSize(600, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	//Add the hexagons to the screen
+	public void populate(){
+		
+	}
 }
